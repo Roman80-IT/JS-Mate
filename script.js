28d50d2bd6cbea867747c1a47d70273e964ee5f0
Roman <jrj80mail@gmail.com>
2023-08-21 16:29:50 +0300

    create

---
+++ script.js
@@ -0,0 +1,22 @@
+//! -----------------------  Task-1  `Simple JS Program`  ------------------------
+
+// I have 10$ amount
+let amount = 20;
+
+// Cookies cost 7$ cookiesPrice
+// Candies cost 8$
+let cookiesPrice = 7;
+let candiesPrice = 8;
+
+// Can I buy both candies and cookies?
+let totalPrice = cookiesPrice + candiesPrice;
+
+if (amount < totalPrice) {
+  console.log("No, I need more money");
+} else {
+  console.log("Yes, I can buy everything");
+}
+// Yes, I can buy everything
+// No, I need more money
+
+//! -----------------------  Task-2  ------------------------
